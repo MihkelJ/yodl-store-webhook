466e9e642c7e3b93d40828659a4f2555848829d3
mihkel <joll.mihkeel@gmail.com>
2025-05-09 10:38:34 -0300

    chore: update package version and enhance README with quick start guide and environment variable details

--- src/routes/txWebhook.routes.ts
+++ src/routes/txWebhook.routes.ts
@@ -1,12 +1,10 @@
 import { defaultEndpointsFactory } from 'express-zod-api';
 import createHttpError from 'http-errors';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
-import { config } from '../config/index.js';
 import authMiddleware from '../middlewares/auth.middlewares.js';
 import txValidationMiddleware from '../middlewares/validation.middlewares.js';
 import { statusResponseSchema } from '../schemas/common.schemas.js';
 import { txInputSchema } from '../schemas/tx.schemas.js';
-import { openBeerTap } from '../services/blynk.service.js';
 
 export const txWebhook = defaultEndpointsFactory
   .addMiddleware(authMiddleware)
@@ -16,10 +14,11 @@ export const txWebhook = defaultEndpointsFactory
     handler: async ({ options: { beerValue } }) => {
       try {
         try {
-          await openBeerTap({
-            token: config.beerTap.token,
-            value: beerValue,
-          });
+          console.log('Opening beer tap');
+          // await openBeerTap({
+          //   token: config.beerTap.token,
+          //   value: beerValue,
+          // });
           return {
             status: ReasonPhrases.OK,
           };
