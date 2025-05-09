f73805e9f0e8e00a7a46b6532f26d31b9efb6fa2
mihkel <joll.mihkeel@gmail.com>
2025-05-09 14:13:14 -0300

    feat: enhance txWebhook route to open beer tap with error handling and logging

--- src/routes/txWebhook.routes.ts
+++ src/routes/txWebhook.routes.ts
@@ -1,10 +1,12 @@
 import { defaultEndpointsFactory } from 'express-zod-api';
 import createHttpError from 'http-errors';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { config } from '../config/index.js';
 import authMiddleware from '../middlewares/auth.middlewares.js';
 import txValidationMiddleware from '../middlewares/validation.middlewares.js';
 import { statusResponseSchema } from '../schemas/common.schemas.js';
 import { txInputSchema } from '../schemas/tx.schemas.js';
+import { openBeerTap } from '../services/blynk.service.js';
 
 export const txWebhook = defaultEndpointsFactory
   .addMiddleware(authMiddleware)
@@ -14,15 +16,24 @@ export const txWebhook = defaultEndpointsFactory
     handler: async ({ options: { beerValue }, logger }) => {
       try {
         try {
-          logger.info('Opening beer tap');
-          // await openBeerTap({
-          //   token: config.beerTap.token,
-          //   value: beerValue,
-          // });
+          const response = await openBeerTap({
+            token: config.beerTap.token,
+            value: beerValue,
+          });
+
+          if (response.status !== 200) {
+            logger.error('Failed to open beer tap', { response });
+            throw createHttpError(
+              StatusCodes.INTERNAL_SERVER_ERROR,
+              ReasonPhrases.INTERNAL_SERVER_ERROR
+            );
+          }
+
           return {
             status: ReasonPhrases.OK,
           };
         } catch (error) {
+          logger.error('Failed to open beer tap', { error });
           throw createHttpError(
             StatusCodes.INTERNAL_SERVER_ERROR,
             ReasonPhrases.INTERNAL_SERVER_ERROR
