fd553a641acc490daf92307e02917e94e5d4009b
mihkel <joll.mihkeel@gmail.com>
2025-07-10 13:57:46 -0300

    feat: add transaction service for fetching payment details by transaction hash

---
+++ src/services/transaction.service.ts
@@ -0,0 +1,16 @@
+import createHttpError from 'http-errors';
+import { ReasonPhrases } from 'http-status-codes';
+import { config } from '../config/index.js';
+import { Payment, TransactionResponse } from '../types/transaction.js';
+
+export async function fetchTransaction(txHash: string): Promise<Payment> {
+  const response = await fetch(`${config.yodl.indexerUrl}/v1/payments/${txHash}`);
+
+  if (!response.ok) {
+    throw createHttpError(response.status, ReasonPhrases.INTERNAL_SERVER_ERROR);
+  }
+
+  const { payment } = (await response.json()) as TransactionResponse;
+
+  return payment;
+}
